4ca40435de58b0e7502eada22725ca6d22584a68
Laura Kapitza <laura.kapitza@gmail.com>
2023-03-18 23:06:09 +0100

    Fix Render postgres url issue

--- config.py
+++ config.py
@@ -5,8 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL", "postgresql://localhost:5432/dance")
+if app.config['SQLALCHEMY_DATABASE_URI'].startswith("postgres://"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI'].replace("postgres://", "postgresql://")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+# TODO:
+#  DONT FORGET TO CLEAR THE DB AND
+#  RUN flask db migrate
